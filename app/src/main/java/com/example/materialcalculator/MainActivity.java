3f112dbeeb190b9743b02e93233c85430e96614f
Christian Royal <croyal04@gmail.com>
2019-06-28 09:26:30 -0400

    Added toast to onCreate

--- app/src/main/java/com/example/materialcalculator/MainActivity.java
+++ app/src/main/java/com/example/materialcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
 
 // intialize button
         btnC=findViewById(R.id.buttonC);
